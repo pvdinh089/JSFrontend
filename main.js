249c26e4acac1897c68ef87aa7a838554f5da188
Dinh Pham Van <pvdinh089@gmail.com>
2020-07-12 21:55:17 +0700

    update

---
+++ main.js
@@ -0,0 +1,27 @@
+var name = 'Pham Van Dinh';
+var age = 30;
+//Hiển thị thông báo trên trình duyệt
+//alert(name + ' ' + age);
+
+// Đây là comment 1 dòng
+
+/**
+ * Đây là 
+ * commnents
+ * nhiều dòng
+ */
+
+ // Một số phương thức của đối tượng console
+console.warn('Đây là dạng in ra cảnh báo - màu vàng');
+console.error('Đây là cảnh báo lỗi - màu đỏ');
+console.log('In thông báo bình thường');
+
+//Hàm này có 2 đối số, 1 đối số là hàm 
+// setTimeout(function(){
+//     console.log('Hello');
+// }, 1000);
+
+// setInterval(function(){
+//     console.log('Hello World' + ' ' + Math.random());
+// },1000)
+
---
+++ operators.js
@@ -0,0 +1,29 @@
+/** Toán tủ số học
++   --> Cộng
+-   --> Trừ
+*   --> Nhân
+**  --> Lũy thừa
+/   --> Chia
+%   --> Chia lấy dư
+++  --> Tăng 1 giá trị số
+--  --> Giảm 1 giá trị số
+ */
+
+var a = 1;
+var b = 2;
+console.log("a = 1");
+console.log("b = 2");
+
+console.log("Phép cộng: " + (a + b));
+console.log("Phép trừ: " + (a - b));
+console.log("Phép nhân " + a * b);
+console.log("Phép lũy thừa: " + a ** b);
+console.log("Phép chia: " + a / b);
+console.log("Phép chia lấy dư: " + (a % b));
+
+a++;
+console.log("Tăng a lên 1 với a++ :" + a);
+
+b--;
+console.log("Giảm b đi 1 với b-- :" + b);
+// Có thể làm bài tập nâng cao với các bài tính diện tích
